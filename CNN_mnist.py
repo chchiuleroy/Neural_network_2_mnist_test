313d7d406ed0bd12f8c49c326a19a3c50a8725e5
GameLogist <36932180+GameLogist@users.noreply.github.com>
2018-10-03 00:02:03 +0530

    Maybe try Sparse Categorical Crossentropy?

--- CNN_mnist.py
+++ CNN_mnist.py
@@ -30,10 +30,10 @@ model.add(MaxPooling2D(pool_size = (2, 2)))
 model.add(Dropout(0.25))
 model.add(Flatten())
 model.add(Dense(128, activation = "relu"))
-model.add(Dropout(0.5))
+model.add(Dropout(0.3))
 model.add(Dense(10, activation = "softmax"))
 print(model.summary())
-model.compile(loss = "categorical_crossentropy", optimizer = "adam", metrics = ["accuracy"])
+model.compile(loss = "sparse_categorical_crossentropy", optimizer = "adam", metrics = ["accuracy"])
 train_history = model.fit(x = x_train4D_nor, y = y_train_oh, validation_split = 0.2, epochs = 12, batch_size = 300, verbose = 2)
  
 def show_train_history(train_history, train, validation):  
